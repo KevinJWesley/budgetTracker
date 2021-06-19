081cf369af6a33a47165a028842f9fc21718b0d4
kevinjwesley <kevin.john.wesley@gmail.com>
2021-06-18 19:24:52 -0600

    working on indexedDB functionality in db.js

--- public/db.js
+++ public/db.js
@@ -1,4 +1,5 @@
 let db;
+
 // open "budgetTracker" in indexedDB
 const request = indexedDB.open("budgetTrackerDB", 1);
 
@@ -15,11 +16,60 @@ request.onupgradeneeded = (event) => {
 };
 
 request.onsuccess = function (event) {
-  const transaction = db.transaction(["budgetTrackerDB"], "readwrite");
-  const budgetStore = transaction.objectStore("budgetTrackerDB");
-  budgetStore.add(event);
+  db = event.target.result;
+  //check if app is back online
+  if (navigator.onLine) {
+    console.log("Backend Online");
+    checkDatabase();
+  }
 };
 
-// function checkDatabase() {}
+request.onerror = function (event) {
+  // log error here
+  console.log("error");
+};
 
-// window.addEventListener("online", checkDatabase);
+function saveRecord(record) {
+  // create a transaction on the pending db with readwrite access
+  const transaction = db.transaction(["budgetTrackerDB"], "readwrite");
+  // access your pending object store
+  const budgetStore = transaction.objectStore("budgetTrackerDB");
+
+  // add record to your store with add method.
+  budgetStore.add(record);
+}
+
+function checkDatabase() {
+  // open a transaction on your pending db
+  const transaction = db.transaction(["budgetTrackerDB"], "readwrite");
+  // access your pending object store
+  const budgetStore = transaction.objectStore("budgetTrackerDB");
+
+  // get all records from store and set to a variable
+  const allRecords = budgetStore.getAll();
+
+  allRecords.onsuccess = function () {
+    if (allRecords.result.length > 0) {
+      fetch("/api/transaction/bulk", {
+        method: "POST",
+        body: JSON.stringify(allRecords.result),
+        headers: {
+          Accept: "application/json, text/plain, */*",
+          "Content-Type": "application/json",
+        },
+      })
+        .then((response) => response.json())
+        .then(() => {
+          // if successful, open a transaction on your pending db
+          const transaction = db.transaction(["budgetTrackerDB"], "readwrite");
+          // access your pending object store
+          const budgetStore = transaction.objectStore("budgetTrackerDB");
+
+          // clear all items in your store
+          budgetStore.clear();
+        });
+    }
+  };
+}
+
+window.addEventListener("online", checkDatabase);
